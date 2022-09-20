36f13cb9665a85d83fd7fe125517dd82304851ae
Jess Archer <jess@jessarcher.com>
2022-09-20 16:31:06 +1000

    Nvim packer

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,3 @@
+require('user/plugins')
 require('user/options')
 require('user/keymaps')
---
+++ nvim/lua/user/plugins.lua
@@ -0,0 +1,43 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+require('packer').reset()
+require('packer').init({
+  compile_path = vim.fn.stdpath('data')..'/site/plugin/packer_compiled.lua',
+  --display = {
+  --  open_fn = function()
+  --    return require('packer.util').float({ border = 'solid' })
+  --  end,
+  --},
+})
+
+local use = require('packer').use
+
+-- Packer can manage itself.
+use('wbthomason/packer.nvim')
+
+-- Commenting support.
+use('tpope/vim-commentary')
+
+-- Automatically set up your configuration after cloning packer.nvim
+-- Put this at the end after all plugins
+if packer_bootstrap then
+    require('packer').sync()
+end
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile>
+  augroup end
+]])
